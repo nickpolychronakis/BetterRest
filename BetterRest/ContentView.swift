c0092de0bb09aed7eebfeeccac02fd7a1fc8a6d0
Nick Polychronakis <nickpolychronakis@me.com>
2019-10-20 19:35:08 +0300

    Ready (VStack version)

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,10 +9,109 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Binding properties
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    // MARK: - Alert properties
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    // MARK: - View body
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                // MARK: WakeUp time
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                }
+                // MARK: Desired sleep
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper(value: $sleepAmount, in: 4...12) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                // MARK: Coffee intake
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(value: $coffeeAmount, in: 1...20) {
+                        if coffeeAmount == 1 {
+                            Text("1 cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime){
+                    Text("Calculate")
+                }
+            )
+            .alert(isPresented: $showingAlert) { () -> Alert in
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
+        }
     }
+    
+    
+    // MARK: - Default wake time
+    /// The default date time to be used with datePicker
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
+    
+    // MARK: - Bed time (ML)
+    /// Calculates and shows the best time to go to bed, based on ML data NOT PURE
+    func calculateBedtime() {
+        // Initializing ML
+        let model = SleepCalculator()
+        // Converting wakeUp time to seconds
+        let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            // Predictions of ML
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            // Finding sleep time
+            let sleepTime = wakeUp - prediction.actualSleep
+            // Formatter
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = formatter.string(from: sleepTime)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Your machine took a day off"
+        }
+        showingAlert = true
+    }
+    
+    
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
